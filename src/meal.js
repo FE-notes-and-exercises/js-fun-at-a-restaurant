cc2a6182cb76feb64f27851737252ab2dd936c41
Yuji Kosakowski <yuji.kosakowski@gmail.com>
2023-08-11 14:51:17 -0600

    Add functions to meal.js Co-authored-by: Gabe Nunez gabriel.c.nunez@gmail.com

--- src/meal.js
+++ src/meal.js
@@ -1,15 +1,45 @@
-function nameMenuItem() {
-
+function nameMenuItem(name) {
+  return `Delicious ${name}`
 }
 
+function createMenuItem(menuItemName, price, type) {
+  return {
+    name: menuItemName,
+    price: price,
+    type: type
+  }
+}
+
+function addIngredients(item, ingredients) {
+  if (!ingredients.includes(item)) {
+    ingredients.push(item)
+  }
+}
+
+function formatPrice(initialPrice) {
+    return `$${initialPrice}`
+    // return '$' + `${initialPrice}`
+}
+
+function decreasePrice(menuItemPrice) {
+  return menuItemPrice * .9;
+}
+
+function createRecipe(title, ingredients, menuItemType) {
+  return {
+    title: title,
+    ingredients: ingredients,
+    type: menuItemType
+  }
+}
 
 module.exports = {
   nameMenuItem,
-  // createMenuItem,
-  // addIngredients,
-  // formatPrice,
-  // decreasePrice,
-  // createRecipe
+  createMenuItem,
+  addIngredients,
+  formatPrice,
+  decreasePrice,
+  createRecipe
 }
 
 
